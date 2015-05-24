eaabc5d5ed0fd98044e3297575aad94891f1c0ee
Tobias Koppers <tobias.koppers@googlemail.com>
2015-05-24 02:18:34 +0200

    Initial commit

---
+++ bin/index.js
@@ -0,0 +1,44 @@
+var path = require("path");
+var fs = require("fs");
+var prompt = require("prompt");
+var argv = require("minimist")(process.argv.slice(2));;
+var GHOrgOverview = require("../");
+
+var configFile = path.resolve(__dirname, "../config.json");
+
+function loadConfiguration(callback) {
+	fs.exists(configFile, function(exist) {
+		if(exist) {
+			callback(null, require(configFile));
+			return;
+		} else {
+			prompt.get(["token", "org"], function(err, result) {
+				var config = {
+					token: result.token,
+					org: result.org
+				};
+				fs.writeFile(configFile, JSON.stringify(config), function(err) {
+					if(err) return callback(err);
+					callback(null, config);
+				});
+			});
+		}
+	});
+}
+
+loadConfiguration(function(err, configuration) {
+	if(err) {
+		console.error(err);
+		return;
+	}
+	var api = new GHOrgOverview(configuration);
+	api.load(function(err) {
+		if(err) {
+			console.error(err);
+			return;
+		}
+		console.log(api.toString({
+			colors: require("supports-color")
+		}));
+	});
+});
